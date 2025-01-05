a1da274fb48c0bbfac43e8b783d4210e0cfb610a
paleswan1 <pallu.sth1@gmail.com>
2025-01-05 13:01:48 +0545

    UI Fixes and Manipulations.

--- PersonalExpenseTracker/Components/Layout/MainLayout.razor.cs
+++ PersonalExpenseTracker/Components/Layout/MainLayout.razor.cs
@@ -1,4 +1,5 @@
 ﻿using MudBlazor;
+using Shadow = MudBlazor.Shadow;
 using PersonalExpenseTracker.DTOs.Authentication;
 
 namespace PersonalExpenseTracker.Components.Layout;
@@ -16,6 +17,35 @@ public partial class MainLayout
         ZIndex = new ZIndex
         {
             Drawer = 1300
+        },
+        PaletteLight = new PaletteLight()
+        {
+            Primary = "#F1973C",
+            PrimaryLighten = "#FFE8C8",
+            Secondary = "#005399",
+            Success = "#00cc29",
+            Error = "#ff0000",
+            Tertiary = "#ff00001a",
+            TertiaryContrastText = "#ff0000",
+            TertiaryDarken = "#fff",
+            Info = "#0bc5ea",
+            Background = "#f8f8fa",
+            AppbarBackground = "#fff",
+            AppbarText = "#141414",
+            DrawerBackground = "#fff",
+            DrawerText = "rgba(0,0,0, 0.7)",
+            TableLines = "#ebebeb",
+            OverlayDark = "hsl(0deg 0% 0% / 75%)",
+            Divider = "#ebebeb",
+            TextPrimary = "#141414",
+            TextSecondary = "#5c5c5c",
+            GrayLight = "#858585",
+            White = "#fff"
+        },
+        Shadows = new Shadow(),
+        LayoutProperties = new LayoutProperties
+        {
+            DefaultBorderRadius = "5px"
         }
     };
 
